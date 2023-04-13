7388dbff08afa26911cfed4646482f125c2ca1b1
Lorencini <arthnilo@gmail.com>
2023-04-13 11:20:10 -0300

    correção

--- js/slide.js
+++ js/slide.js
@@ -1,36 +1,34 @@
 export default class Slide {
-    constructor(slide, wrapper) {
-        this.slide = document.querySelector(slide);
-        this.wrapper = document.querySelector(wrapper);
-    }
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
 
-    onStart(event) {
-        event.preventDefault();
-        this.wrapper.addEventListener("mousemove", this.onMove)
-    }
+  onStart(event) {
+    event.preventDefault();
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
 
-    onMove(event) {
-    }
+  onMove(event) {}
 
-    onEnd() {
-        this.wrapper.removeEventListener("mousemove", this.onMove)
+  onEnd() {
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+  }
 
-    }
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
 
-    addSlideEvents() {
-        this.wrapper.addEventListener("mousedown", this.onStart)
-        this.wrapper.addEventListener("mouseup", this.onEnd)
-    }
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
 
-    bindEvents() {
-        this.onStart = this.onStart.bind(this);
-        this.onMove = this.onMove.bind(this);
-        this.onEnd = this.onEnd.bind(this);
-    }
-
-    init() {
-        this.bindEvents();
-        this.addSlideEvents();
-        return this;
-    }
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
 }
