c9d410a16cb260128f4f1d0ae242f889b0b34067
Lorencini <arthnilo@gmail.com>
2023-04-13 11:18:49 -0300

    atualização-main-script-slide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-import debounce from "./debounce.js"
+import debounce from "./debounce.js";
+import Slide from "./slide.js";
+
+const slide = new Slide(".slide", ".wrapper");
+slide.init()
+console.log(slide)
