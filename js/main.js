04b938647352e2039282bc867c2e26e192354394
Lorencini <arthnilo@gmail.com>
2023-04-14 08:08:07 -0300

    adicionado-scroll-suave

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@ const slide = new Slide(".slide", ".wrapper");
 slide.init()
 console.log(slide)
 
-slide.changeSlide(3)
+slide.changeSlide(3);
+slide.activePrevSlide();
--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,10 @@ export default class Slide {
     };
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -31,6 +35,7 @@ export default class Slide {
     }
     event.preventDefault();
     this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -43,6 +48,19 @@ export default class Slide {
     const moveType = (event.type ==="mouseup") ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active)
+    }
+    console.log(this.dist.movement)
   }
 
   addSlideEvents() {
@@ -88,8 +106,19 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position;
   }
 
+  activePrevSlide() {
+    if (this.index.prev !== undefined)
+      this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined)
+      this.changeSlide(this.index.next);
+  }
+
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     return this;
