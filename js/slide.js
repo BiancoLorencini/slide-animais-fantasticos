a13a6949bb059a45bf0d15b9914bb51dfbf9ce18
Lorencini <arthnilo@gmail.com>
2023-04-14 11:05:41 -0300

    slide-detalhes-e-finalização

--- js/slide.js
+++ js/slide.js
@@ -157,7 +157,7 @@ export class Slide {
 }
 //Extends
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
     this.bindControlEvents();
