5e3b5b6e5b8449c65878793f00d9dd25e62725f9
Lorencini <arthnilo@gmail.com>
2023-04-13 23:02:35 -0300

    iniciando config slide

--- js/slide.js
+++ js/slide.js
@@ -2,14 +2,31 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0
+    }
+  }
+
+  moveSlide(distX) {
+this.slide.style.transform = 
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = this.dist.startX - clientX;
+    return this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
-  onMove(event) {}
+  onMove(event) {
+    const finalPosition =  this.updatePosition(event.clientX)
+  }
 
   onEnd() {
     this.wrapper.removeEventListener("mousemove", this.onMove);
